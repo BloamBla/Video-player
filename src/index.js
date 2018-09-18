ef4b08e8402ec0ed034b02809cfd3e239db78bc4
Stanislav Khromazhenkov <staslife1994@gmail.com>
2018-09-18 17:13:11 +0300

    some changes
    - was restructured JS code;
    - corrected page layout for mobile devices.

--- src/index.js
+++ src/index.js
@@ -3,55 +3,80 @@ import '../styles/main.scss'
 
 window.addEventListener('load', onLoad);
 
+let data = [];
+
+let linkData = new XMLHttpRequest();
+linkData.open('GET', '../data.json'); // async=true
+linkData.onload = function () {
+    if (linkData.readyState === 4 && linkData.status === 200) {
+        data = JSON.parse(linkData.responseText);
+    }
+};
+linkData.send(null);
+
 function onLoad() {
-    let data = require('../data.json');
+
     let chosenMovie = data[0];
-    const videoTag = document.querySelector('#video');
-    const chosenSource = document.querySelector('#source');
+
+    let template = document.querySelector('#movies');
+
     const chosenTitle = document.querySelector('#chosenTitle');
     const chosenDirector = document.querySelector('#chosenDirector');
     const chosenDescription = document.querySelector('#chosenDescription');
-    const links = {chosenTitle, chosenDirector, chosenDescription, chosenSource};
+    const chosenSource = document.querySelector('#source');
+    const videoTag = document.querySelector('#video');
 
-    function setMovie(){
-        if (typeof(chosenMovie.video) !== 'object'){
-            links.chosenSource.setAttribute('src', 'src/' + chosenMovie.video);
-        } else links.chosenSource.setAttribute('src', 'src/video/' + chosenMovie.video[0]);
-        videoTag.setAttribute('poster', 'src/img/' + chosenMovie.preview);
-        videoTag.load();
-        links.chosenTitle.textContent = chosenMovie.title;
-        links.chosenDirector.textContent = chosenMovie.director;
-        links.chosenDescription.textContent = chosenMovie.description;
+    const links = {chosenTitle, chosenDirector, chosenDescription, chosenSource, videoTag};
+
+    function setMovie(links, movie) {
+        if (typeof(movie.video) !== 'object') {
+            links.chosenSource.setAttribute('src', 'src/' + movie.video);
+        } else links.chosenSource.setAttribute('src', 'src/video/' + movie.video[0]);
+        links.videoTag.setAttribute('poster', 'src/img/' + movie.preview);
+        links.videoTag.load();
+        links.chosenTitle.textContent = movie.title;
+        links.chosenDirector.textContent = movie.director;
+        links.chosenDescription.textContent = movie.description;
     }
 
-    setMovie();
-
-    let template = document.querySelector('#movies');
-    for (let i = 0; i < data.length; i++) {
-        let movieInfo = data[i];
-        let clone = template.content.cloneNode(true);
-        let image = clone.querySelector('.template-item-back');
-        let title = clone.querySelector('p');
-        title.textContent = movieInfo.title;
-        image.setAttribute('style', 'background: url(src/img/' + movieInfo.preview + ') no-repeat center; background-size: contain;');
-        template.parentNode.appendChild(clone);
+    function setMoviesBlocks(templateLink, moviesData) {
+        for (let i = 0; i < moviesData.length; i++) {
+            let movieInfo = moviesData[i];
+            let clone = templateLink.content.cloneNode(true);
+            let image = clone.querySelector('.template-item-back');
+            let title = clone.querySelector('h5');
+            title.textContent = movieInfo.title;
+            image.setAttribute('style', 'background: url(src/img/' + movieInfo.preview + ') no-repeat center; background-size: contain;');
+            templateLink.parentNode.appendChild(clone);
+        }
     }
 
-    let videoChoise = document.querySelectorAll('.template-item');
+    setMovie(links, chosenMovie);
+    setMoviesBlocks(template, data);
 
-    for (let i = 0; i < videoChoise.length; i++) {
-        videoChoise[i].onclick = function () {
-            let newMovie = [];
-            for (let j = 0; j < videoChoise.length; j++) {
-                if (videoChoise[i].textContent.trim() === data[j].title) {
-                    newMovie = data[j];
-                }
+    let videoChoice = document.querySelectorAll('.template-item');
+
+    function changeMovie(arr, i, lastChosenMovie, moviesData, video) {
+        let newMovie = [];
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i].textContent.trim() === moviesData[j].title) {
+                newMovie = moviesData[j];
             }
-            if (newMovie !== chosenMovie) {
-                chosenMovie = newMovie;
-                setMovie();
-                video.play();
-            }
+        }
+        if (newMovie !== lastChosenMovie) {
+            lastChosenMovie = newMovie;
+            setMovie(links, lastChosenMovie);
+            video.play();
+        } else {
+            setMovie(links, lastChosenMovie);
+            video.load();
+            video.play();
+        }
+    }
+
+    for (let i = 0; i < videoChoice.length; i++) {
+        videoChoice[i].onclick = function () {
+            changeMovie(videoChoice, i, chosenMovie, data, videoTag);
         }
     }
 }
